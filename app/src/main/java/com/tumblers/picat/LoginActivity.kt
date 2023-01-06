8102121f786d813f885aecb44756d6e12fd5cdaa
sumin <ysm2706@naver.com>
2023-01-06 14:16:03 +0900

    feat:카카오 로그인 정보 수정

--- app/src/main/java/com/tumblers/picat/LoginActivity.kt
+++ app/src/main/java/com/tumblers/picat/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.tumblers.picat
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +13,8 @@ import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import com.tumblers.picat.databinding.ActivityLoginBinding
 import com.kakao.sdk.auth.LoginClient
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +30,8 @@ class LoginActivity : AppCompatActivity() {
         // println("해시 : ${keyHash}")
         // 해시 : FQv64xfMYFfJQNT9Edn9wbiFueI=
 
-        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+        // 토큰 정보 보기
+       UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
             if (error != null) {
                 Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
             }
@@ -87,9 +91,29 @@ class LoginActivity : AppCompatActivity() {
 //            startActivity(intent)
 //            finish()
 
-            if(LoginClient.instance.isKakaoTalkLoginAvailable(this)){
-                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
-            }else{
+//            if(LoginClient.instance.isKakaoTalkLoginAvailable(this)){
+//                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
+//            }else{
+//                LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
+//            }
+            if (LoginClient.instance.isKakaoTalkLoginAvailable(this)) {
+                LoginClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
                 LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
             }
 
