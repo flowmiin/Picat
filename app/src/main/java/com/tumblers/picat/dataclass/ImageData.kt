0aa8735bc97bf83c512c27c155186242d0cf851d
sumin <ysm2706@naver.com>
2023-01-07 18:07:07 +0900

    feat : 사진 전송시 list로 변경해서 전송

--- app/src/main/java/com/tumblers/picat/dataclass/ImageData.kt
+++ app/src/main/java/com/tumblers/picat/dataclass/ImageData.kt
@@ -1,18 +1,12 @@
 package com.tumblers.picat.dataclass
 
 import com.google.gson.annotations.SerializedName
-import okhttp3.MultipartBody
 
 data class ImageData(
-    //request members
-    val arrayImage: MutableList<MultipartBody.Part>,
-    val img_cnt: Int,
-
-    //response members
-    val img_list: ArrayList<Data>
+    val url: List<String>,
+    val img_cnt: Int
 )
 
 class Data(
     @SerializedName("url") var url: String,
-    @SerializedName("img_cnt") var img_cnt: Int
 )
