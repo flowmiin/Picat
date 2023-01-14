a9ea719f6c4765e85a721e3a068c270e6c2add91
sumin <ysm2706@naver.com>
2023-01-15 00:27:11 +0900

    feat : 다이얼로그 크기 수정

--- app/src/main/java/com/tumblers/picat/InviteDialog.kt
+++ app/src/main/java/com/tumblers/picat/InviteDialog.kt
@@ -35,6 +35,8 @@ class InviteDialog(private val context : AppCompatActivity) {
         dialog.requestWindowFeature((Window.FEATURE_ACTION_BAR)) // 액션 바 제거
         dialog.setContentView(binding.root)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//        dialog.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT)
+//        dialog.setCanceledOnTouchOutside(false)
         dialog.setCancelable(false) // 다이얼로그으 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
 
         binding.inviteCheckButton.setOnClickListener {
